8f2ab1ff45042e5aa05648fde2f733aad27c71fc
Juan Manuel Gonzalez <juanmg0511@users.noreply.github.com>
2022-06-01 14:26:44 -0300

    Remove avatar from get all users

--- src/users.py
+++ src/users.py
@@ -253,7 +253,9 @@ class User(Resource):
                 "first_name": existingUser["first_name"],
                 "last_name": existingUser["last_name"],
                 "contact": existingUser["contact"],
-                "avatar": existingUser["avatar"],
+                # Usar el endpoint dedicado para obtener el avatar de
+                # los usuarios
+                "avatar": "...",
                 "login_service": existingUser["login_service"],
                 "account_closed": existingUser["account_closed"],
                 "date_created": existingUser["date_created"],
