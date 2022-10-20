5fe1f601e805ade1bf0533dfb76ba57e0c55b49e
Juan Manuel Gonzalez <juanmg0511@users.noreply.github.com>
2022-10-20 17:10:02 -0300

    Save only response status in request log

--- src/requestlog.py
+++ src/requestlog.py
@@ -72,17 +72,6 @@ def log_request(response):
        and ("/password" in data["path"])):
         data["value"] = "*"
 
-    # Parseamos la response a un diccionario
-    try:
-        # No logueamos las respuestas de pedidos de log!
-        if (("requestlog" in path)
-           and (response.status_code == HTTPStatus.OK)):
-            resp_data = "*"
-        else:
-            resp_data = eval(str(response.data.decode("utf-8")))
-    except Exception:
-        resp_data = str(response.data.decode("utf-8"))
-
     # Armamos el documento a guardar en la base de datos
     requestLog = {
         "log_type": "request",
@@ -93,12 +82,11 @@ def log_request(response):
         "api_version": "v" + config.api_version,
         "method": method,
         "path": path,
-        "status": status,
         "duration": duration,
         "headers": headers,
         "args": args,
         "data": data,
-        "response": resp_data
+        "response_status": status
     }
 
     # Insertamos el registro en la base de datos
